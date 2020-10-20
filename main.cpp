c77f129f8024c9bf4962edf97c358e6f538caff3
Ivan Mikerin <vmik7@yandex.ru>
2020-10-20 11:14:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,12 +17,17 @@ private:
 public:
 	// Конструкторы со списками инициализации
 	my_vector(int _x = 0, int _y = 0, int _z = 0) : x(_x), y(_y), z(_z) {}
-   	my_vector(int x_0, int y_0, int z_0, int x_1, int y_1, int z_1) : x(x_1 - x_0), y(y_1 - y_0), z(z_1 - z_0) {}
+    my_vector(int x_0, int y_0, int z_0, int x_1, int y_1, int z_1) : x(x_1 - x_0), y(y_1 - y_0), z(z_1 - z_0) {}
 	
 	// Доступ к координатам вектора
 	int get_x() const { return this->x; }
 	int get_y() const { return this->y; }
 	int get_z() const { return this->z; }
+	
+	// Изменение координат
+	int set_x(int value) { this->x = value; }
+	int set_y(int value) { this->y = value; }
+	int set_z(int value) { this->z = value; }
 	
 	// Получение длины вектора
 	double length() const {
@@ -52,21 +57,56 @@ public:
     
 int main() {
 	
-	my_vector a, b(3, 4, 5), c(1, 2, 3, 4, 5, 6);
+	int x_0, y_0, z_0, x_1, y_1, z_1;
 	
-	cout << a.get_x() << " " << a.get_y() << " " << a.get_z() << " " << a.length() << endl;
-	cout << b.get_x() << " " << b.get_y() << " " << b.get_z() << " " << b.length() << endl;
-	cout << c.get_x() << " " << c.get_y() << " " << c.get_z() << " " << c.length() << endl;
+	cout << "Введите конечные точки первого вектора: " << endl;
 	
-	my_vector d = b + c;
-	cout << d.get_x() << " " << d.get_y() << " " << d.get_z() << " " << d.length() << endl;
+	cout << "x[0]: ";
+	cin >> x_0;
+	cout << "y[0]: ";
+	cin >> y_0;
+	cout << "z[0]: ";
+	cin >> z_0;
 	
-	my_vector e = c - b;
-	cout << e.get_x() << " " << e.get_y() << " " << e.get_z() << " " << e.length() << endl;
+	cout << "x[1]: ";
+	cin >> x_1;
+	cout << "y[1]: ";
+	cin >> y_1;
+	cout << "z[1]: ";
+	cin >> z_1;
 	
-	cout << b * c << endl;
+	my_vector a(x_0, y_0, z_0, x_1, y_1, z_1);
 	
-	cout << b.cos(b) << endl;
+	cout << "Введите конечные точки второго вектора: " << endl;
+	
+	cout << "x[0]: ";
+	cin >> x_0;
+	cout << "y[0]: ";
+	cin >> y_0;
+	cout << "z[0]: ";
+	cin >> z_0;
+	
+	cout << "x[1]: ";
+	cin >> x_1;
+	cout << "y[1]: ";
+	cin >> y_1;
+	cout << "z[1]: ";
+	cin >> z_1;
+	
+	my_vector b(x_0, y_0, z_0, x_1, y_1, z_1);
+	
+	cout << "\nКоординаты первого вектора:\n";
+	cout << a.get_x() << " " << a.get_y() << " " << a.get_z() << endl;
+	cout << "Длина: " << a.length() << endl;
+	
+	cout << "\nКоординаты второго вектора:\n";
+	cout << b.get_x() << " " << b.get_y() << " " << b.get_z() << endl;
+	cout << "Длина: " << b.length() << endl;
+	
+	my_vector sum = a + b;
+	cout << "\nКоординаты суммы этих векторов:\n";
+	cout << sum.get_x() << " " << sum.get_y() << " " << sum.get_z() << endl;
+	cout << "Длина: " << sum.length() << endl;
 	
     return 0;
 }
